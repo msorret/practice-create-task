e8c7fe865b6c4ae9bb64b0131c42a397ddaf5498
Ms. Orret <orret.deborah@pusd.us>
2023-09-12 20:10:37 +0000

    Period 5 In Class Practice Task

---
+++ period5.py
@@ -0,0 +1,24 @@
+restaurants = ["In N Out", "McDonalds", "Chick Fil A", "Jack in the Box", "Taco Bell"]
+
+new_res = input("What restaurant would you like to rank into your list? ")
+
+def rank_restaurant(new_res, restaurants):
+
+    for i in range(len(restaurants)):
+
+        rank = input("Do you like A) " + new_res + " more or B) "+ restaurants[i] + " more? A/B")
+
+        if rank == "A":
+            restaurants.insert(i, new_res)
+            break
+        elif rank == "B":
+            continue
+    
+    if new_res not in restaurants:
+        restaurants.append(new_res)
+
+    return restaurants
+
+print("Your new ranking of restaurants is ", rank_restaurant(new_res, restaurants))
+
+
